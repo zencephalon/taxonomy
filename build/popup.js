8374531c5132b6507fc5fe58247935c786e65e52
Matthew Bunday <zenceph@gmail.com>
2016-09-01 17:50:33 -0400

    A lot of progress, stuck on getting keybindings to work the way I want

--- build/popup.js
+++ build/popup.js
@@ -1,124 +1,152 @@
 'use strict';
 
-// Traverse the bookmark tree, and print the folder and nodes.
-function dumpBookmarks(query) {
-  var bookmarkTreeNodes = chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
-    $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-  });
-}
-function dumpTreeNodes(bookmarkNodes, query) {
-  var list = $('<ul>');
-  var i;
-  for (i = 0; i < bookmarkNodes.length; i++) {
-    list.append(dumpNode(bookmarkNodes[i], query));
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SELECT = 'SELECT';
+var SORT = 'SORT';
+var EDIT = 'EDIT';
+
+var Taxonomy = function () {
+  function Taxonomy() {
+    var _this = this;
+
+    _classCallCheck(this, Taxonomy);
+
+    this.folderTitle = function (title) {
+      return title === '' ? 'root' : title;
+    };
+
+    this.backShortcutHtml = function (f) {
+      return f.parentId ? 'Backspace: go back up' : '';
+    };
+
+    this.selectFolder = function (id) {
+      if (_this.state === SELECT) {
+        _this.folderId = id;
+        _this.renderSelect();
+      }
+    };
+
+    this.getFolders = function (nodes) {
+      return nodes.filter(function (n) {
+        return !n.url;
+      });
+    };
+
+    this.getShortcut = function (folderId) {
+      return _this.shortcutsById[folderId];
+    };
+
+    this.state = SELECT;
+    this.folderId = '0';
+    this.$app = $('#app');
+    this.shortcuts = {};
+    this.shortcutsById = {};
   }
-  return list;
-}
-function dumpNode(bookmarkNode, query) {
-  if (bookmarkNode.title) {
-    if (query && !bookmarkNode.children) {
-      if (String(bookmarkNode.title).indexOf(query) == -1) {
-        return $('<span></span>');
+
+  _createClass(Taxonomy, [{
+    key: 'renderSelect',
+    value: function renderSelect() {
+      var _this2 = this;
+
+      chrome.bookmarks.getSubTree(this.folderId, function (_ref) {
+        var _ref2 = _slicedToArray(_ref, 1);
+
+        var f = _ref2[0];
+
+        console.log(f);
+        var fs = _this2.getFolders(f.children);
+        _this2.resetShortcuts();
+        _this2.addShortcuts(fs);
+        _this2.addBackShortcut(f);
+        _this2.renderHtml('<h2>current folder: ' + _this2.folderTitle(f.title) + '</h2>' + ('<ul>' + fs.map(function (tf) {
+          return '<li>' + _this2.getShortcut(tf.id) + ': ' + tf.title + '</li>';
+        }).join('') + '</ul>') + _this2.backShortcutHtml(f));
+      });
+    }
+  }, {
+    key: 'resetShortcuts',
+    value: function resetShortcuts() {
+      Mousetrap.reset();
+      this.shortcuts = {};
+      this.shortcutsById = {};
+    }
+  }, {
+    key: 'addBackShortcut',
+    value: function addBackShortcut(f) {
+      var _this3 = this;
+
+      if (f.parentId) {
+        Mousetrap.bind('backspace', function () {
+          return _this3.selectFolder(f.parentId);
+        });
       }
     }
-    var anchor = $('<a>');
-    anchor.attr('href', bookmarkNode.url);
-    anchor.text(bookmarkNode.title);
-    /*
-     * When clicking on a bookmark in the extension, a new tab is fired with
-     * the bookmark url.
-     */
-    anchor.click(function () {
-      chrome.tabs.create({ url: bookmarkNode.url });
-    });
-    var span = $('<span>');
-    var options = bookmarkNode.children ? $('<span>[<a href="#" id="addlink">Add</a>]</span>') : $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' + 'href="#">Delete</a>]</span>');
-    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' + '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' + '</td></tr></table>') : $('<input>');
-    // Show add and edit links when hover over.
-    span.hover(function () {
-      span.append(options);
-      $('#deletelink').click(function () {
-        $('#deletedialog').empty().dialog({
-          autoOpen: false,
-          title: 'Confirm Deletion',
-          resizable: false,
-          height: 140,
-          modal: true,
-          overlay: {
-            backgroundColor: '#000',
-            opacity: 0.5
-          },
-          buttons: {
-            'Yes, Delete It!': function YesDeleteIt() {
-              chrome.bookmarks.remove(String(bookmarkNode.id));
-              span.parent().remove();
-              $(this).dialog('destroy');
-            },
-            Cancel: function Cancel() {
-              $(this).dialog('destroy');
-            }
-          }
-        }).dialog('open');
+  }, {
+    key: 'addShortcuts',
+    value: function addShortcuts(folders) {
+      var _this4 = this;
+
+      folders.forEach(function (folder) {
+        var shortcut = _this4.shortestRemainingShortcut(folder.title);
+        var id = folder.id;
+
+        _this4.shortcuts[shortcut] = id;
+        _this4.shortcutsById[id] = shortcut;
+        Mousetrap.bind(shortcut.split('').join(' ') + ' space', function () {
+          return _this4.selectFolder(id);
+        });
       });
-      $('#addlink').click(function () {
-        $('#adddialog').empty().append(edit).dialog({ autoOpen: false,
-          closeOnEscape: true, title: 'Add New Bookmark', modal: true,
-          buttons: {
-            'Add': function Add() {
-              chrome.bookmarks.create({ parentId: bookmarkNode.id,
-                title: $('#title').val(), url: $('#url').val() });
-              $('#bookmarks').empty();
-              $(this).dialog('destroy');
-              window.dumpBookmarks();
-            },
-            'Cancel': function Cancel() {
-              $(this).dialog('destroy');
-            }
-          } }).dialog('open');
-      });
-      $('#editlink').click(function () {
-        edit.val(anchor.text());
-        $('#editdialog').empty().append(edit).dialog({ autoOpen: false,
-          closeOnEscape: true, title: 'Edit Title', modal: true,
-          show: 'slide', buttons: {
-            'Save': function Save() {
-              chrome.bookmarks.update(String(bookmarkNode.id), {
-                title: edit.val()
-              });
-              anchor.text(edit.val());
-              options.show();
-              $(this).dialog('destroy');
-            },
-            'Cancel': function Cancel() {
-              $(this).dialog('destroy');
-            }
-          } }).dialog('open');
-      });
-      options.fadeIn();
-    },
-    // unhover
-    function () {
-      options.remove();
-    }).append(anchor);
-  }
-  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
-  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-    li.append(dumpTreeNodes(bookmarkNode.children, query));
-  }
-  return li;
-}
+    }
+  }, {
+    key: 'shortestRemainingShortcut',
+    value: function shortestRemainingShortcut(t) {
+      var title = t.toLowerCase();
+      for (var i = 2; i <= title.length; i++) {
+        var proposed = title.slice(0, i);
+        if (!this.shortcuts[proposed]) {
+          return proposed;
+        }
+      }
+      return title;
+    }
+  }, {
+    key: 'renderHtml',
+    value: function renderHtml(html) {
+      this.$app.html(html);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      switch (this.state) {
+        case SELECT:
+          this.renderSelect();
+          break;
+        default:
+          break;
+      }
+    }
+  }]);
+
+  return Taxonomy;
+}();
 
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.bookmarks.search("Lifehacking", function (array) {
-    console.log(array);
-  });
-  chrome.bookmarks.getTree(function (array) {
-    console.log(array);
-  });
+  var taxonomy = new Taxonomy();
+  taxonomy.render();
+
+  // chrome.bookmarks.getSubTree('0', bookmarkTreeNode => console.log(bookmarkTreeNode))
+  // chrome.bookmarks.getSubTree('1', bookmarkTreeNode => console.log(bookmarkTreeNode))
+  // chrome.bookmarks.search('Lifehacking', (array) => { console.log(array) })
+  // chrome.bookmarks.getTree((array) => { console.log(array) })
   Mousetrap.bind('space a a', function () {
-    console.log("aa");
+    console.log('aa');
   });
   Mousetrap.bind('space a b', function () {
-    console.log("ab");
+    console.log('ab');
   });
 });
--- src/popup.js
+++ src/popup.js
@@ -6,17 +6,97 @@ class Taxonomy {
   constructor() {
     this.state = SELECT
     this.folderId = '0'
+    this.$app = $('#app')
+    this.shortcuts = {}
+    this.shortcutsById = {}
+  }
+
+  folderTitle = title => (title === '' ? 'root' : title)
+
+  backShortcutHtml = f => (f.parentId ? 'Backspace: go back up' : '')
+
+  renderSelect() {
+    chrome.bookmarks.getSubTree(this.folderId, ([f]) => {
+      console.log(f)
+      const fs = this.getFolders(f.children)
+      this.resetShortcuts()
+      this.addShortcuts(fs)
+      this.addBackShortcut(f)
+      this.renderHtml(
+        `<h2>current folder: ${this.folderTitle(f.title)}</h2>` +
+        `<ul>${fs.map(tf => `<li>${this.getShortcut(tf.id)}: ${tf.title}</li>`).join('')}</ul>` +
+        this.backShortcutHtml(f)
+      )
+    })
+  }
+
+  selectFolder = (id) => {
+    if (this.state === SELECT) {
+      this.folderId = id
+      this.renderSelect()
+    }
+  }
+
+  resetShortcuts() {
+    Mousetrap.reset()
+    this.shortcuts = {}
+    this.shortcutsById = {}
+  }
+
+  addBackShortcut(f) {
+    if (f.parentId) {
+      Mousetrap.bind('backspace', () => this.selectFolder(f.parentId))
+    }
+  }
+
+  addShortcuts(folders) {
+    folders.forEach(folder => {
+      const shortcut = this.shortestRemainingShortcut(folder.title)
+      const { id } = folder
+      this.shortcuts[shortcut] = id
+      this.shortcutsById[id] = shortcut
+      Mousetrap.bind(`${shortcut.split('').join(' ')} space`, () => this.selectFolder(id))
+    })
+  }
+
+  getFolders = (nodes) => nodes.filter(n => !n.url)
+
+  getShortcut = (folderId) => this.shortcutsById[folderId]
+
+  shortestRemainingShortcut(t) {
+    const title = t.toLowerCase()
+    for (let i = 2; i <= title.length; i++) {
+      const proposed = title.slice(0, i)
+      if (!this.shortcuts[proposed]) {
+        return proposed
+      }
+    }
+    return title
+  }
+
+  renderHtml(html) {
+    this.$app.html(html)
+  }
+
+  render() {
+    switch (this.state) {
+      case SELECT:
+        this.renderSelect()
+        break
+      default:
+        break
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const taxonomy = new Taxonomy()
-  taxonomy.init()
+  taxonomy.render()
 
-  chrome.bookmarks.getSubTree('0', bookmarkTreeNode => console.log(bookmarkTreeNode))
-  chrome.bookmarks.getSubTree('1', bookmarkTreeNode => console.log(bookmarkTreeNode))
-  chrome.bookmarks.search('Lifehacking', (array) => { console.log(array) })
-  chrome.bookmarks.getTree((array) => { console.log(array) })
+  // chrome.bookmarks.getSubTree('0', bookmarkTreeNode => console.log(bookmarkTreeNode))
+  // chrome.bookmarks.getSubTree('1', bookmarkTreeNode => console.log(bookmarkTreeNode))
+  // chrome.bookmarks.search('Lifehacking', (array) => { console.log(array) })
+  // chrome.bookmarks.getTree((array) => { console.log(array) })
   Mousetrap.bind('space a a', () => { console.log('aa') })
   Mousetrap.bind('space a b', () => { console.log('ab') })
 });
